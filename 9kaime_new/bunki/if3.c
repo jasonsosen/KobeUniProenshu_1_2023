9407854128bb4347e194b2b894b205ecbc2a5988
mzk-komatsu <103207651+mzk-komatsu@users.noreply.github.com>
2023-06-14 10:57:50 +0900

    add 9kaime_new

---
+++ 9kaime_new/bunki/if3.c
@@ -0,0 +1,18 @@
+// if3.c
+#include <stdio.h>
+
+int main(void){
+  int ;
+  printf("年齢を入力してください\n");
+  scanf( );
+  
+  if (){
+  
+  }else if(){
+  
+  }else{
+  
+  }
+  
+	return 0;
+}
---
+++ 9kaime_new/enzan/renshu9_1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(void){
+  int x, y;
+  printf("二つの整数を入力せよ．\n");
+  printf("整数x："); scanf("%d", &x);
+  printf("整数y："); scanf("%d", &y);
+
+  printf("x+y=%d\n", x+y);	//和
+  printf("x-y=%d\n", x-y);	//差
+  printf("x*y=%d\n", x*y);	//積
+  printf("x/y=%d\n", x/y);	//商
+  printf("x%%y=%d\n",x%y); //余り
+  return 0;
+}
